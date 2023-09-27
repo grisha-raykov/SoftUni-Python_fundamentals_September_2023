a06aafa39ed66513aba673fe657c1c9ca9031c1a
abruski <abruski@gmail.com>
2023-09-27 07:37:04 +0300

    new exercises

---
+++ 05-basic syntax, conditional statements and loops/05-Lab/07-patterns.py
@@ -0,0 +1,15 @@
+number = int(input())
+
+for i in range(0, number+1):
+    print('*'*i)
+for j in range(number - 1, 0, -1):
+    print('*'*j)
+
+# for i in range(1, number+1):
+#     for j in range(0, i):
+#         print('*', end='')
+#     print()
+# for i in range(number -1, 0, -1):
+#     for j in range(0, i):
+#         print('*', end='')
+#     print()
