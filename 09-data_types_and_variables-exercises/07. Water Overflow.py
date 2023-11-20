08b0ecb42bc9ccf78df4a2a52a963a3ac82f49ad
abruski <abruski@gmail.com>
2023-11-20 06:58:01 +0200

    add new exercises

--- 09-data_types_and_variables-exercises/07. Water Overflow.py
+++ 09-data_types_and_variables-exercises/07. Water Overflow.py
@@ -1,7 +1,14 @@
 number_of_lines = int(input())
 
 tank_capacity = 255
+litres_counter = 0
 
-litress_sum = 0
+
 for current_line in range(number_of_lines):
     litres = int(input())
+    if tank_capacity - litres < 0: #Tank is too small
+        print('Insufficient capacity!')
+        continue
+    tank_capacity -= litres
+    litres_counter += litres
+print(litres_counter)
